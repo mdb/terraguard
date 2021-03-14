584587d571d459955c46664f5617f3ca19151667
Mike Ball <mikedball@gmail.com>
2021-03-14 17:52:42 -0400

    add additional check test scenarios

--- cmd/terraguard/check_test.go
+++ cmd/terraguard/check_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"os/exec"
 	"strings"
 	"testing"
@@ -10,21 +9,25 @@ import (
 
 func TestCheck(t *testing.T) {
 	tests := []struct {
-		arg     string
-		outputs []string
-		err     error
+		description string
+		args        []string
+		outputs     []string
+		err         error
 	}{{
-		arg:     "--foo",
-		outputs: []string{"Error: unknown flag: --foo\n"},
-		err:     errors.New("exit status 1"),
+		description: "when passed an unknown '--foo' flag",
+		args:        []string{"--foo"},
+		outputs:     []string{"Error: unknown flag: --foo\n"},
+		err:         errors.New("exit status 1"),
 	}, {
-		arg: "foo",
+		description: "when passed an unknown 'foo' flag",
+		args:        []string{"foo"},
 		outputs: []string{
 			"Error: required flag(s) \"plan\", \"resources\" not set",
 		},
 		err: errors.New("exit status 1"),
 	}, {
-		arg: "--help",
+		description: "when passed '--help'",
+		args:        []string{"--help"},
 		outputs: []string{
 			"Check if a Terraform plan seeks to modify the specified resources",
 			"Usage:",
@@ -32,7 +35,8 @@ func TestCheck(t *testing.T) {
 		},
 		err: nil,
 	}, {
-		arg: "-h",
+		description: "when passed '-h'",
+		args:        []string{"-h"},
 		outputs: []string{
 			"Check if a Terraform plan seeks to modify the specified resources",
 			"Usage:",
@@ -40,32 +44,56 @@ func TestCheck(t *testing.T) {
 		},
 		err: nil,
 	}, {
-		arg: "",
+		description: "when passed no arguments",
+		args:        []string{""},
 		outputs: []string{
 			"Error: required flag(s) \"plan\", \"resources\" not set",
 		},
 		err: errors.New("exit status 1"),
+	}, {
+		description: "when the plan JSON file does not exist",
+		args: []string{
+			"--plan=does_not_exist.json",
+			"--resources=foo",
+		},
+		outputs: []string{
+			"Error: open does_not_exist.json: no such file or directory",
+		},
+		err: errors.New("exit status 1"),
+	}, {
+		description: "when the plan JSON does not indicate changes to the specified resource",
+		args: []string{
+			"--plan=../../test_fixtures/basic_plan.json",
+			"--resources=foo",
+		},
+		outputs: []string{
+			"",
+		},
+		err: nil,
 	}}
 
 	for _, test := range tests {
-		t.Run(fmt.Sprintf("when terraguard is passed '%s'", test.arg), func(t *testing.T) {
-			output, err := exec.Command("./terraguard", "check", test.arg).CombinedOutput()
+		t.Run(test.description, func(t *testing.T) {
+			args := []string{}
+			args = append([]string{"check"}, test.args...)
+
+			output, err := exec.Command("./terraguard", args...).CombinedOutput()
 
 			if test.err == nil && err != nil {
-				t.Errorf("expected '%s' not to error; got '%v'", test.arg, err)
+				t.Errorf("expected '%s' not to error; got '%v'", args, err)
 			}
 
 			if test.err != nil && err == nil {
-				t.Errorf("expected '%s' to error with '%s', but it didn't error", test.arg, test.err.Error())
+				t.Errorf("expected '%s' to error with '%s', but it didn't error", args, test.err.Error())
 			}
 
 			if test.err != nil && err != nil && test.err.Error() != err.Error() {
-				t.Errorf("expected '%s' to error with '%s'; got '%s'", test.arg, test.err.Error(), err.Error())
+				t.Errorf("expected '%s' to error with '%s'; got '%s'", args, test.err.Error(), err.Error())
 			}
 
 			for _, o := range test.outputs {
 				if !strings.Contains(string(output), o) {
-					t.Errorf("expected '%s' to include output '%s'; got '%s'", test.arg, o, output)
+					t.Errorf("expected '%s' to include output '%s'; got '%s'", args, o, output)
 				}
 			}
 		})
